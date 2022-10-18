71fc26a422d6db5f189eec096eb802d66998b5f3
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-18 15:19:35 +0900

    :white_check_mark: build.gradle 수정
    # description
    - Querydsl Setting 후 동작 확인

---
+++ src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,18 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,43 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Commit // Test 에서 @Transactional 이 붙으면 자동 Rollback 이 되기 때문에 공부할 때 @Commit 을 추가해서 데이터를 보는 것도 좋다.
 class QuerydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager entityManager;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		entityManager.persist(hello);
+
+		JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+		QHello qHello = new QHello("h");
+
+		Hello result = queryFactory
+			.selectFrom(qHello)
+			.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
+
+
 
 }
